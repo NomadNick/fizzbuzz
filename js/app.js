ecc1f086b1d0d2ef2511ddf3e95629f420fef4df
NomadNick <emailnick123@gmail.com>
2015-02-23 19:17:55 -0800

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+	for(var i = 1; i < 101; i++) {
+		if(i % 3 === 0 && i % 5 === 0) {
+			$('body').append('FizzBuzz' + ' ');
+		}
+		else if(i % 3 === 0) {
+			$('body').append('Fizz' + ' ');
+		}
+		else if(i % 5 === 0) {
+			$('body').append('Buzz' + ' ');
+		}
+		else {
+			$('body').append(i + ' ')
+		}
+	}
+});
