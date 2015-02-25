c35c06f97a9dac58173a7a779ac419290843accb
NomadNick <emailnick123@gmail.com>
2015-02-24 22:03:34 -0800

    added prompt for user input, refactored function

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
-	for(var i = 1; i < 101; i++) {
+
+	var stringEntry = prompt('Please enter a number!');
+	var numEntry = +stringEntry;
+
+	var fizzBuzz = function() {
+		for(var i = 1; i < numEntry; i++) {
 		if(i % 3 === 0 && i % 5 === 0) {
 			$('body').append('FizzBuzz' + ' ');
 		}
@@ -12,5 +17,21 @@ $(document).ready(function () {
 		else {
 			$('body').append(i + ' ')
 		}
+		}
 	}
+
+	if(isNaN(numEntry) == true) {
+		alert('Please make sure you have entered a number correctly.');
+		return stringEntry;
+	}
+
+	else if(numEntry % 1 != 0) {
+		alert('Please only enter whole numbers without decimals.');
+		return stringEntry;
+	}
+	else {
+		fizzBuzz(numEntry);
+	}
+
+
 });
